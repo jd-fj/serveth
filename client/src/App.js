f3e00591533fb2ac6ee7f28629095a3f78ab2468
ecl_mbp <ecl@ecl-mbps-MacBook-Pro.local>
2022-10-14 21:25:01 -0700

    revert to working play button

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState(null);
+  const [playing, setPlaying] = useState(false);
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
-  async function getData() {
-    fetch("/api")
+  async function playFile() {
+    fetch("/play")
       .then((res) => res.json())
       // .then((data) => setData(data));
       .then((data) => console.log(data));
     return data;
   }
   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await playFile();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Play</button>
+    </form>
     </>
   );
 }
