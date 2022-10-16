d6cd757133f20cc9cce312a8fd112884564e59bd
ecl_mbp <ecl@ecl-mbps-MacBook-Pro.local>
2022-10-15 20:32:01 -0700

    get full data on audio files

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [statData, setStatData] = useState(null);
+  const [statsData, setStatsData] = useState(null);
+  const [playbackData, setPlayData] = useState(null);
   // const [playing, setPlaying] = useState(false);
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    runStat();
+    runStats();
+  }, []);
 
   async function playFile() {
     fetch("/play")
       .then((res) => res.json())
-      // .then((data) => setData(data));
-      .then((data) => console.log(data));
-    return data;
+      .then((data) => setPlayData(data));
+      // .then((data) => console.log("HUH?!", datplaybackData));
+    return playbackData;
   }
 
   async function runStat() {
     fetch("/stat")
       .then((res) => res.json())
-      .then((data) => setData(data.message))
-      console.log("data??? ", data)
-    return data;
+      .then((data) => setStatData(data.stat))
+  }
+  async function runStats() {
+    fetch("/stats")
+      .then((res) => res.json())
+      .then((data) => setStatsData(data.stats))
   }
   
   // handle play
@@ -33,7 +39,7 @@ function App() {
       console.log(err);
     }
   };
-  const handleStop = async (e) => {
+  const triggerStatCommand = async (e) => {
     e.preventDefault();
     try {
       await runStat();
@@ -44,55 +50,71 @@ function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button 
-        style={{
-          fontSize: 230,
-          color: "rgb(247, 227, 243)",
-          fontFamily: "courier",
-          // textEmphasis: "triangle filled yellow",
-          // textEmphasis: "bold",
-          backgroundColor: "rgb(190, 127, 55)",
-          padding: "3%",
-          fontWeight: "bolder",
-          // border: "5px solid rgb(51, 31, 31)"
-          border: "none",
-          backgroundImage: 
-            "linear-gradient(rgb(237, 169, 187), rgb(207, 196, 233)",
-          boxShadow: "2px 2px 1px #f700ff30, 10px 10px 6px rgb(255, 254, 214)"
-        }}
-        type="submit">
-          Play
-        </button>
-      </form>
+    <div>
+        <form onSubmit={handleSubmit}>
+          <button 
+          style={{
+            fontSize: 100,
+            color: "rgb(247, 227, 243)",
+            fontFamily: "courier",
+            // textEmphasis: "triangle filled yellow",
+            // textEmphasis: "bold",
+            backgroundColor: "rgb(190, 127, 55)",
+            padding: "3%",
+            fontWeight: "bolder",
+            // border: "5px solid rgb(51, 31, 31)"
+            border: "none",
+            backgroundImage: 
+              "linear-gradient(rgb(237, 169, 187), rgb(207, 196, 233)",
+            boxShadow: "2px 2px 1px #f700ff30, 10px 10px 6px rgb(255, 254, 214)"
+          }}
+          type="submit">
+            Play
+          </button>
+        </form>
 
-      <form onSubmit={handleStop}>
-        <button 
-        style={{
-          fontSize: 230,
-          color: "rgb(247, 227, 243)",
-          fontFamily: "courier",
-          // textEmphasis: "triangle filled yellow",
-          // textEmphasis: "bold",
-          backgroundColor: "rgb(190, 127, 55)",
-          padding: "3%",
-          fontWeight: "bolder",
-          // border: "5px solid rgb(51, 31, 31)"
-          border: "none",
-          backgroundImage: 
-            "linear-gradient(rgb(237, 169, 187), rgb(207, 196, 233)",
-          boxShadow: "2px 2px 1px #f700ff30, 10px 10px 6px rgb(255, 254, 214)"
-        }}
-        type="submit">
-          Stop
-        </button>
-      </form>
-      {data && 
-        data.map((stat) =>
-          <ul>
-            <li>{stat}</li>
-          </ul>
-        )}
+        <form onSubmit={triggerStatCommand}>
+          <button 
+          style={{
+            fontSize: 100,
+            color: "rgb(247, 227, 243)",
+            fontFamily: "courier",
+            // textEmphasis: "triangle filled yellow",
+            // textEmphasis: "bold",
+            backgroundColor: "rgb(190, 127, 55)",
+            padding: "3%",
+            fontWeight: "bolder",
+            // border: "5px solid rgb(51, 31, 31)"
+            border: "none",
+            backgroundImage: 
+              "linear-gradient(rgb(237, 169, 187), rgb(207, 196, 233)",
+            boxShadow: "2px 2px 1px #f700ff30, 10px 10px 6px rgb(255, 254, 214)"
+          }}
+          type="submit">
+            Stat
+          </button>
+        </form>
+      </div>
+
+
+{/* --------------------STATS--------------------------- */}
+      <div >
+        <h1>Stats Data</h1>
+          {statsData && 
+            statsData.map((stat) =>
+              <ul>
+                <li>{stat}</li>
+              </ul>
+          )}
+        <h1>Stat Data</h1>
+          {statData && 
+            statData.map((stat) =>
+              <ul>
+                <li>{stat}</li>
+              </ul>
+          )}
+      </div>
+{/* --------------------STATS--------------------------- */}
     </>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,39 +1,24 @@
 const express = require("express");
 const sox = require('sox');
 const { exec } = require('node:child_process');
-const { stdout } = require("node:process");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get("/play", (req, res) => {
-  // const deets = getFileInfo(); 
-  // console.log("return these deets: ", deets);
-  // res.json(deets);
-  // res.json({ message: "Hello from server!" });
-  const soxCommand = "play test.mp3";
-  runCommand(soxCommand);
+  exec("play test.mp3 trim 0 3", (error, stdout, stderr) => {
+    if (error) {
+      console.log('Error: ', error);
+    }
+    if (stderr) {
+      console.log('Stderr: ', stderr);
+    }
+    console.log('errything good here')
+    res.send({message: "playing test.mp3"});
+  })
+  console.log("end of /play func")
 });
-
-// app.get("/stop", async (req, res) => {
-//   // const deets = getFileInfo(); 
-//   // console.log("return these deets: ", deets);
-//   // res.json(deets);
-//   // res.json({ message: "Hello from server!" });
-//   const soxCommand = "soxi -r test.mp3";
-//   const huh = runCommand(soxCommand);
-//   const resolveHuh = Promise.resolve(huh)
-//   console.log('huh:', huh)
-//   console.log('resolveHuh:', resolveHuh)
-
-
-//     resolveHuh.then((value) => {
-//       console.log("value: ", value);
-//       // expected output: 123
-//     });
-//   res.json({})
-// });
 
 app.get("/stat", async (req, res) => {
   exec("sox test.mp3 -n stat", (error, stdout, stderr) => {
@@ -43,12 +28,26 @@ app.get("/stat", async (req, res) => {
     }
     if (stderr) {
       console.log("stderr: ", stderr)
-      const stat = stderr.trim().split("\n")
-      res.send({message: stat})
+      const soxOutput = stderr.trim().split("\n")
+      res.send({stat: soxOutput})
     }
-    console.log(`am i even working??? ${stdout}`);
 
-    res.json({message: stdout});
+    // res.json({message: stdout});
+  });
+});
+app.get("/stats", async (req, res) => {
+  exec("sox test.mp3 -n stats", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log("stderr: ", stderr)
+      const soxOutput = stderr.trim().split("\n")
+      res.send({stats: soxOutput})
+    }
+
+    // res.json({message: stdout});
   });
 });
 
@@ -56,18 +55,14 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-async function runCommand(command) {
-  const returneth = exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    console.log(`am i even working??? ${stdout}`);
-    // console.log(' am i even working!!! ',stdout)
-    const endOfRunCommand = Promise.resolve(stdout);
-    // return resolve(stdout);
-    // return endOfRunCommand
-    return stdout;
-  });
-  return returneth;
-}
+// async function runCommand(command) {
+//   exec(command, (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(`error: ${error.message}`);
+//       return;
+//     }
+//     console.log(`am i even working??? ${stdout}`);
+//     return stdout;
+//   });
+//   return returneth;
+// }
